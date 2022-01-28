753ce000bc3016e7a07eb065d6ad857732e697f0
Cesar Torres <torres-cesar@outlook.com>
2022-01-27 22:48:27 -0600

    Assign player choice from button click

--- js/script.js
+++ js/script.js
@@ -1,51 +1,47 @@
+function getWinner(playerSelection, computerSelection) {
+    if (playerSelection == "rock" && computerSelection == "rock") {
+        return "It's a tie! Try again.";
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+        return "You lose! Paper beats rock.";
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        return "You win! Rock beats Scissors.";
+    }
+  
+    if (playerSelection == "paper" && computerSelection == "rock") {
+        return "You win! Paper beats rock.";
+    } else if (playerSelection == "paper" && computerSelection == "paper") {
+        return "It's a tie! Try again.";
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        return "You lose! Scissors beats paper.";
+    }
+  
+    if (playerSelection == "scissors" && computerSelection == "rock") {
+        return "You lose! Rock beats scissors.";
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return "You win! Scissors beats paper.";
+    } else if (playerSelection == "scissors" && computerSelection == "scissors"){
+        return "It's a tie! Try again.";
+    }
+}
+
 function computerSelection() {
-  let randomNumber = Math.random();
-
-  if (randomNumber < 0.33) {
-      return "rock";
-  } else if (randomNumber < 0.66) {
-      return "paper";
-  } else {
-      return "scissors";
-  }
+    let randomNumber = Math.random();
+    if (randomNumber < 0.33) {
+        return "rock";
+    } else if (randomNumber < 0.66) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
 }
 
-function playRound(playerSelection, computerSelection) {
-
-  if (playerSelection === "rock" && computerSelection === "rock") {
-      return "It's a tie! Try again.";
-  } else if (playerSelection === "rock" && computerSelection === "paper") {
-      return "You lose! Paper beats rock.";
-  } else if (playerSelection === "rock" && computerSelection === "scissors") {
-      return "You win! Rock beats Scissors.";
-  }
-
-  if (playerSelection === "paper" && computerSelection === "rock") {
-      return "You win! Paper beats rock.";
-  } else if (playerSelection === "paper" && computerSelection === "paper") {
-      return "It's a tie! Try again.";
-  } else if (playerSelection === "paper" && computerSelection === "scissors") {
-      return "You lose! Scissors beats paper.";
-  }
-
-  if (playerSelection === "scissors" && computerSelection === "rock") {
-      return "You lose! Rock beats scissors.";
-  } else if (playerSelection === "scissors" && computerSelection === "paper") {
-      return "You win! Scissors beats paper.";
-  } else {
-      return "It's a tie! Try again.";
-  }
-}
-
-
-function playerSelection() {
-    const buttonRock = document.querySelector(".rock");
-    const buttonPaper = document.querySelector(".paper");
-    const buttonScissors = document.querySelector(".scissors");
-
-    buttonRock.addEventListener("click", () => "rock")
-    buttonPaper.addEventListener("click", () => "paper")
-    buttonScissors.addEventListener("click", () => "scissors")
-}
-
-console.log(playRound());
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        let playerChoice = button.classList.toString();
+        let computerChoice = computerSelection();
+        console.log(playerChoice);
+        console.log(computerChoice);
+        console.log(getWinner(playerChoice, computerChoice));
+    })
+})
